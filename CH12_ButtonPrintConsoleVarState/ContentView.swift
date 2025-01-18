7080cc52753b28e8e2801ca02402607385e7bbfb
Kevin Lefebvre <kevin@gbamerica.net>
2025-01-18 14:19:13 -0500

    When using State variables keep them private if only needed on one file. Variable wil not show up in other files Lower right to bringup console widow; print("Hello") CMD+K to clear console Tab key to move between default arguments of function Press return when highlighted argument. (Default Button action) When switch to Trailing Closure format

--- CH12_ButtonPrintConsoleVarState/ContentView.swift
+++ CH12_ButtonPrintConsoleVarState/ContentView.swift
@@ -8,12 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    //var message = "I Am A Programmer!" // property of view. Can't change in body
+    @State var message = "I Am A Programmer!" // can be changed in body
+    // state variables stay with view.
+    //@State private var message = "I Am A Programmer!"
+    // private - only accessible in this file
+    // will not show up in other files. If only needed in one file. Do this.
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+            Button("Click me!") {
+                message = "Awesome!"
+            }
         }
         .padding()
     }
